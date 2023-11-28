3961b63ca3c984aad09b683fb6285ee842343dd8
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-11-28 02:16:09 -0300

    first commit

---
+++ src/main/java/com/ms/user/dtos/EmailDto.java
@@ -0,0 +1,43 @@
+package com.ms.user.dtos;
+
+import java.util.UUID;
+
+public class EmailDto {
+    private UUID userUid;
+    private String emailTo;
+    private String subject;
+    private String text;
+
+    public UUID getUserUid() {
+        return userUid;
+    }
+
+    public void setUserUid(UUID userUid) {
+        this.userUid = userUid;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
